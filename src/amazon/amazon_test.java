e06b699b4fd778b0b0c2d792b695ac267b33689a
Hyacinth Ashley Radovan <ashleyradovan28@gmail.com>
2024-07-05 20:58:51 +0800

    Second commit

--- src/amazon/amazon_test.java
+++ src/amazon/amazon_test.java
@@ -1,10 +1,18 @@
 package amazon;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -22,11 +30,37 @@ public class amazon_test {
     }
     
     @Test
-    public void Amazon() {
+    public void AmazonHome() {
+        // Navigate to Amazon
         driver.get("https://amazon.in");
-        driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys("Keyboards");
-        driver.findElement(By.xpath("//*[@id=\"nav-flyout-searchAjax\"]/div[2]/div/div[1]/div[4]/div/div[1]")).click();
+
+        // Locate the search box and perform a search
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("Iphone 12");
+        driver.findElement(By.id("nav-search-submit-button")).click();
+
+        // Click on the specific product link
+        driver.findElement(By.linkText("Apple iPhone 13 (128GB) - Blue")).click();
+
+        // Store window handles in a list
+        Set<String> handles = driver.getWindowHandles();
+        ArrayList<String> array = new ArrayList<>(handles);
+
+        // Print window handles (for debugging purposes)
+        System.out.println("Parent Window Handle: " + array.get(0));
+        System.out.println("Child Window Handle: " + array.get(1));
+
+        // Switch to the new window (child window)
+        driver.switchTo().window(array.get(1));
+
+        // Wait for the button to be clickable
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // Click the "Buy Now" button
+        driver.findElement(By.id("buy-now-button")).click();
+
+        
+        
         
     }
 }
